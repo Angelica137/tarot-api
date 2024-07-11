7dcff8a713785bbea57d0447ed2a504e7f49b392
Angelica <Angelica137@users.noreply.github.com>
2024-07-11 15:27:28 +0100

    reading model, green

--- app/models/reading_model.py
+++ app/models/reading_model.py
@@ -6,4 +6,8 @@ from app import db
 class Reading(db.Model):
     __tablename__ = 'readings'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    question = db.Column(db.String(255), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    spread_id = db.Column(db.Integer, db.ForeignKey('spreads.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
--- tests/models/test_reading_model.py
+++ tests/models/test_reading_model.py
@@ -3,8 +3,27 @@ import pytest
 from app.models.reading_model import Reading
 
 
-def test_reading_model(db):
-    reading = Reading(name='Reading 1')
-    db.session.add(reading)
-    db.session.commit()
-    assert reading.id is not None
+@pytest.fixture
+def sample_reading_data():
+    return {
+        "id": 1,
+        "question": "What do I need to know?",
+        # "user_id": 1,
+        "spread_id": 1,
+        "created_at": "2021-01-01",
+        "updated_at": "2021-01-01",
+    }
+
+
+def test_new_reading(sample_reading_data):
+    """
+    GIVEN a Reading model
+    WHEN a new Reading is created
+    THEN check the id, question, user_id, spread_id, created_at, and updated_at are defined correctly
+    """
+    reading = Reading(**sample_reading_data)
+    assert reading.id == 1
+    assert reading.question == "What do I need to know?"
+    assert reading.spread_id == 1
+    assert reading.created_at == "2021-01-01"
+    assert reading.updated_at == "2021-01-01"
